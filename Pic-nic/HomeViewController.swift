177206c94a7ccdfb38dce11047ce865d9c8d0776
Isaiah Suarez <isaiah.suarez@icloud.com>
2023-03-04 15:08:41 -0600

    Added boilerplate code to Home

--- Pic-nic/HomeViewController.swift
+++ Pic-nic/HomeViewController.swift
@@ -7,5 +7,37 @@
 
 import UIKit
 
+private let cellIdentifier = "PostCell"
+
 class HomeViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableview: UITableView!
+    
+    // TODO: Fill from Firebase
+    let datasource = ["Post 1", "Post 2", "Post 3"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableview.delegate = self
+        tableview.dataSource = self
+    }
+    
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datasource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        return cell
+    }
+    
+    
 }
