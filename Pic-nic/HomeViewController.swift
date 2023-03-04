ed7230a4c296806823c309d70aad5819d283b437
Isaiah Suarez <isaiah.suarez@icloud.com>
2023-03-04 16:41:37 -0600

    Basic outlets working for PostCell

--- Pic-nic/HomeViewController.swift
+++ Pic-nic/HomeViewController.swift
@@ -15,7 +15,7 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var tableview: UITableView!
     
     // TODO: Fill from Firebase
-    let datasource = ["Post 1", "Post 2", "Post 3"]
+    let datasource = [("Evan Chan", "Zilker", "boots.jpg", "8:52pm", "2 likes", "Evan.jpg")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +35,14 @@ extension HomeViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: PostCell.reuseIdentifer, for: indexPath) as! PostCell
+        let post = datasource[indexPath.row]
+        cell.nameLabel.text = post.0
+        cell.locationLabel.text = post.1
+        cell.postImageView.image = UIImage(named: post.2)
+        cell.profileImageView.image = UIImage(named: post.5)
+        cell.timeLabel.text = post.3
+        cell.likesLabel.text = post.4
         return cell
     }
     
