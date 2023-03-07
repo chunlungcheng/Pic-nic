596a2340d1572a9c6f339a08f5c0e333a22acb71
Isaiah Suarez <isaiah.suarez@utexas.edu>
2023-03-07 15:46:31 +0000

    Kevin(camera vc)

---
+++ Pic-nic/View Controllers/CameraViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CameraViewController.swift
+//  Pic-nic
+//
+//  Created by Chun-Lung Cheng on 3/5/23.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func postButtonPressed(_ sender: Any) {
+    }
+
+}
