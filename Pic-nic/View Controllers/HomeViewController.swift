f367874dd870cea2aeb032647fb64ee06763a919
Isaiah Suarez <isaiah.suarez@icloud.com>
2023-04-14 11:28:40 -0500

    basic home

--- Pic-nic/View Controllers/HomeViewController.swift
+++ Pic-nic/View Controllers/HomeViewController.swift
@@ -7,22 +7,67 @@
 
 import UIKit
 import Firebase
+import FirebaseFirestore
 
 class HomeViewController: UIViewController {
     
     @IBOutlet weak var tableview: UITableView!
     
-    // TODO: Fill from Firebase
-    let datasource = [("Evan Chan", "Zilker", "boots.jpg", "8:52pm", "2 likes", "Evan.jpg")]
+    var datasource = [Post]()
+    let db = Firestore.firestore()
+    
+    /// Set to true if user is not logged in when home screen is presented
+    var needsToDownload = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableview.delegate = self
         tableview.dataSource = self
+        downloadPosts()
+    }
+    
+    func downloadPosts() {
+        let postsRef = db.collection("locations").document("Austin").collection("posts") // DO NOT CHANGE
+        postsRef.getDocuments { (snapshot, error) in
+            if let error = error {
+                print("Error fetching documents: \(error)")
+                return
+            }
+            guard let documents = snapshot?.documents else { return }
+            var posts = [Post]()
+            
+            for document in documents {
+                let id = document.documentID
+                let data = document.data()
+                let date = data["date"] as? String ?? ""
+                let imageData = data["imageData"] as? Data
+                let location = data["location"] as? String ?? ""
+                let userID = data["userID"] as? String ?? ""
+                
+                if let imageData = imageData, let image = UIImage(data: imageData) {
+                    posts.append(Post(date: date, image: image, location: location, userID: userID))
+                }
+            }
+            
+            // Do something with the users array
+            print(posts)
+            self.datasource = posts
+            self.tableview.reloadData()
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         checkIfUserIsLoggedIn()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("We back")
+        if needsToDownload {
+            downloadPosts()
+            needsToDownload = false
+        }
     }
     
     func checkIfUserIsLoggedIn() {
@@ -30,9 +75,11 @@ class HomeViewController: UIViewController {
             let storyboard = UIStoryboard(name: "Login", bundle: nil)
             if let loginVC = storyboard.instantiateViewController(withIdentifier: LoginViewController.identifier) as? LoginViewController {
                 loginVC.delegate = self
+                needsToDownload = true
                 present(loginVC, animated: true)
             }
-           
+        } else {
+            print(Auth.auth().currentUser?.email)
         }
     }
 }
@@ -54,12 +101,12 @@ extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: PostCell.reuseIdentifer, for: indexPath) as! PostCell
         let post = datasource[indexPath.row]
-        cell.nameLabel.text = post.0
-        cell.locationLabel.text = post.1
-        cell.postImageView.image = UIImage(named: post.2)
-        cell.profileImageView.image = UIImage(named: post.5)
-        cell.timeLabel.text = post.3
-        cell.likesLabel.text = post.4
+        cell.nameLabel.text = "Name"
+        cell.locationLabel.text = "Location"
+        cell.postImageView.image = post.image
+//        cell.profileImageView.image = UIImage(named: post.5)
+        cell.timeLabel.text = "time"
+        cell.likesLabel.text = "likes"
         return cell
     }
     
